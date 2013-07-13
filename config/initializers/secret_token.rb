d19781535a0c4d785a08f838db812626eaa5076c
robinhood205 <a1217yn@aiit.ac.jp>
2013-07-13 16:48:33 +0900

    0713

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Sample0713::Application.config.secret_token = 'efa8532f6f5c1183457a9a6ea5573ad461f4637e1bb4209c89bdafdd18aa80317d58044f5052f7d20ea9ba857dc4051d88eecac8f7b29d1574188bf35276a7dd'
